9edcd4b4a976c6e4b7358f4bd49f3601247e5e6b
Ekam Sidhu <ekamsinghsidhu@yahoo.com>
2020-08-17 12:43:24 -0700

    Updates

---
+++ precache-manifest.9cdf69e66b59dd6b7c9f0e9e940c319e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d944b1b7089a4dea79cc6b43ac62a2",
+    "url": "/react-restaurant/index.html"
+  },
+  {
+    "revision": "00689b97c0b299eaa830",
+    "url": "/react-restaurant/static/css/main.f729de73.chunk.css"
+  },
+  {
+    "revision": "a8cb655c4e5c85c537be",
+    "url": "/react-restaurant/static/js/2.945602f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-restaurant/static/js/2.945602f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00689b97c0b299eaa830",
+    "url": "/react-restaurant/static/js/main.449e6b73.chunk.js"
+  },
+  {
+    "revision": "d2c132d79a53eb457e1f",
+    "url": "/react-restaurant/static/js/runtime-main.79c8d7e8.js"
+  },
+  {
+    "revision": "3ea510e5342b2d0b4baa82dc1bbf13bc",
+    "url": "/react-restaurant/static/media/fries.3ea510e5.jpeg"
+  }
+]);
